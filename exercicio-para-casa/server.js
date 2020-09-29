121f0136065a171faeba2d0c1e77fd8dbf07ab74
patriciasouzas <patriciasouzaa89@gmail.com>
2020-09-28 22:01:52 -0300

    subindo servidor

---
+++ exercicio-para-casa/server.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+const PORT = 8080
+
+app.listen(PORT, function (request, response) {
+console.log(`The server is running on port ${PORT}`);
+})
