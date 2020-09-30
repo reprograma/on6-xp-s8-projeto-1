16f41e7d37072e9ed53b7ac465980830d8959092
patriciasouzas <patriciasouzaa89@gmail.com>
2020-09-30 19:54:54 -0300

    projeto final

--- exercicio-para-casa/scr/routes/gamesRoutes.js
+++ exercicio-para-casa/scr/routes/gamesRoutes.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/gamesControllers")
 
-router.get("/jogos", controller.getGames)
+router.get("/jogos", controller.getAll)
+router.get("/jogos/:id", controller.getById)
 
 module.exports = router
